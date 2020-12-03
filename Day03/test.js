b9e1bae699ac746817fb66d7a7991de1c37ba2da
ThatRoman <roma151@mail.ru>
2020-12-03 21:08:47 +1100

    day3 part 1 challenge solved

---
+++ Day03/test.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const filename = './Day3inputSlope.txt';
+
+// 1. read *.txt file and convert into array of arrays (matrix)
+
+fs.readFile(filename, 'utf8', function(err, data) {  
+    if (err) throw err;
+
+    const dataArr = data.split('\n');
+    const arrLength =dataArr[0].length;
+    let counter = 0;
+    let index = 0;
+
+    // 2. loop through array of arrays and access [x] element of each subarray
+    // x - being a variable assigned 0 at first which is increased by 3 
+    // each step of .forEach loop below
+    dataArr.forEach((element, i) => {
+
+        // 3. add to the global counter each time the symbol is equals to "#"
+        if(element[index] === "#") {
+            counter++;
+            // console.log(`Found a tree at line ${i}`)
+        }       
+
+        index = index + 3;
+        // 4. check if index reaches the end of a subarray - subtract the length of subarray
+        // from the index in order to simulate infinite forest recplication
+        if(index > element.length - 1){
+            index = index - element.length;
+        }
+    }); 
+    console.log(`Program has found ${counter} trees on your path given the input`)
+ 
+  
+});
