9e637c4dc4be295d6e257ee1fe4d99d03a578d25
ThatRoman <roma151@mail.ru>
2020-12-04 09:59:07 +1100

    2015day2part2 solution added

--- 2015AoC/2015day2part2Solution.js
+++ 2015AoC/2015day2part2Solution.js
@@ -11,14 +11,15 @@ fs.readFile(filename, 'utf8', function(err, data) {
 
     let answer = 0;
 
+
     dataArr.forEach((box, index) => {
         // if (index < 10) { //for debugging limit to first 10 lines
             box = box.split('x').map(Number).sort((function(a,b){return a-b}));
-            const area = box[0]*box[1]*2 + box[1]*box[2]*2 + box[0]*box[2]*2;
-            const areaTotal = area + box[0]*box[1];
+            const wrapRib = box[0]*2 + box[1]*2;
+            const bowRib = box[0]*box[1]*box[2];
             // console.log('for the box: ', box, ' whole area equals to: ', area, ' and with smallest side added: ', areaTotal);
-            answer = answer + areaTotal;
+            answer = answer + wrapRib + bowRib;
         // }
     })
-    console.log('the program has finished calculations.Total area is:', answer)
+    console.log('the program has finished calculations.Total ribbon length is:', answer, 'feet')
 });
