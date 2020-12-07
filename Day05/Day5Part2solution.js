3c721741119f07deb903d820a9f3534f58a5640f
ThatRoman <roma151@mail.ru>
2020-12-08 05:59:38 +1100

    Day5part2solution restructured

--- Day05/Day5Part2solution.js
+++ Day05/Day5Part2solution.js
@@ -9,14 +9,10 @@ fs.readFile(filename, 'utf8', function(err, data) {
     // first we need to parse the data and split into JS array:
     const dataArr = data.split(/\n/g)                           //split by new line character
     
-    let bookingList = {}; // create a JSON to push all found bookings 
-    for (let i=0; i < 128; i++){
-        bookingList[i] = new Array;
-        };
-    // console.log(typeof(bookingList[13]))
+    let bookingList = new Array; // create a list to push all found bookings 
+
     // For each boarding pass (item in dataArr) we need to find row and seat numbers 
     // and multiply before pushing to an empty list
-    listOfBp = [];
     dataArr.forEach(boardingPassRaw => {
         let [nRow, nSeat, rowAcc, seatAcc] = [ 63.5, 3.5, 64, 4 ];
         for (let i = 0; i < 7; i++) {
@@ -40,21 +36,20 @@ fs.readFile(filename, 'utf8', function(err, data) {
                     nSeat = nSeat + seatAcc;
                 }
         } 
-    bookingList[nRow].push(nSeat); //during each loop add to our booking list every seat found
-    // console.log(typeof(bookingList[nRow]))
-    // console.log('For BoardPass:', boardingPassRaw, 'the row number is: ', nRow, 
-    //             'the seat number is: ', nSeat);
-    listOfBp.push((nRow*8)+nSeat)
+    bookingList.push((nRow*8) + nSeat); //during each loop add to our booking list every seat found
     })
     // and finally return the highest from the list of multiplied
-    console.log(Math.max(...listOfBp))
+    // console.log(Math.max(...bookingList))
 
     // this will populate the list of the rows with only 7 seats taken. 
     // Then you have to multiply the row number by 8 and add the missing spot number
-    for (let i=0; i < Object.keys(bookingList).length; i++) {
-        if (bookingList[i].length === 7){
-          console.log(bookingList[i],'missing spots at row #', i)
+    bookingList.sort(function(a, b){return a - b});
+    console.log(bookingList)
+    for (let i=1; i < bookingList.length-1; i++) {
+        if (bookingList[i]+1 != bookingList[i+1]){
+          console.log(bookingList[i],' After <<< found my ticket number is #', bookingList[i]+1)
     
         }
     }
+    
 })
