aaa4aeba0d396024853fc396243c62b3ff37ddb5
ThatRoman <roma151@mail.ru>
2021-01-17 20:43:30 +1100

    Finally day1 solved

--- Day07/Day7part1solution.js
+++ Day07/Day7part1solution.js
@@ -4,67 +4,66 @@ const path = require('path');
 
 const filename = path.join(__dirname, './Day7inputTags.txt');
 
-fs.readFile(filename, 'utf8', function(err, data) {
-    if (err) throw err;
+const adjacencyList = new Map();
 
-    // first we need to parse the data and split into JS object based on 'contains' word:
-    var dataJson = {};
-    data.replace(/\sbags?\s/g,'').split(/\.\n/g).forEach( s => {   // ReGex to get rid of repeating word Bag(s) and spaces around it
-      let [a, b] = s.split('contain');
-        dataJson[a] = b;
-          // console.log(' the current string is ', s)
-      })
-      var giantTree = {};
-      var redundant = 0;
-      // console.log('example of data \'shiny gold\'', dataJson['shiny gold'])
-      for (let key in dataJson) {                 //using destructured keys in dataJson to put into a new Obj
-        if (dataJson.hasOwnProperty(key)) {       // then checking if it even exist
-          if (giantTree[key]){                    // if it already exist in our new Obj -> warn me and add exra counter in order to avoid collision
-            console.error('found redundancy for:', key)
-            redundant++;
-            giantTree[key] = redundant.toString() + destructStr(dataJson[key],dataJson)
-            }
-          else {giantTree[key] = destructStr(dataJson[key],dataJson) }
-        }
-    }
-    // console.log();
-    console.log(giantTree['shiny gold'], '< shiny gold\n', giantTree);
+function addNode(bag) {
+  adjacencyList.set(bag, []);
+}
 
-    // containers.push(...checkForBag(dataJson, 'shiny gold'));
+function addEdge(parent, child){
+  adjacencyList.get(parent).push(child)
+}
 
+const parentalList = new Map();
+
+function addChildNode(bag){
+  parentalList.set(bag, []);
+}
+function addParentToChild(parent, child){
+  if (!parentalList.has(child)){
+    addChildNode(child)
+  }
+
+  parentalList.get(child).push(parent)
+}
+
+tags = fs.readFileSync(filename, 'utf8', function(err, data) {
+  if (err) throw err;      
 })
 
-// Function to break down a string of rules to find all of the possible bags it contains
-const destructStr = function(str, passedArr, acc) {
-  console.log('processing', str)
-  if(!str instanceof String || str.length < 17){console.log('returning: ', str); return str}
-  else {
-    var arr = str.split(', ')
-    // console.log('substrings are:', arr)
-    arr.forEach(subBag => {
-      // let n = Number(subBag.slice(0,2)); // Define the depth for each element (number of sub-bags it can have)
-      if(!str.includes('no other') && subBag){
-        var justColor = passedArr[subBag.replace(/\sbags?/g,'').trim().slice(2)];
-        console.log('inside FOR loop... sending to recursion:', justColor,'//arr length', arr.length, 'current subBag value is', subBag)
-        return str.concat(destructStr(justColor, passedArr));
-      } else {console.log('ELSE RETURN TRUE'); return str.concat('no other')};
-  })
-}
-}
-                                        // Function that takes in JSON and the bag type and spits out list of
-const checkForBag = function(json, bagType){  // found direct holders
-  const tmp = [];
-  tmp.push(bagType);
-  for (let [key, value] of Object.entries(json)) {
-    if (containers.includes(...newBag)){
-      console.log(...newBag, 'is already on the list... skipping')
-    }
-    else {
-      if (value.includes(bagType)) {
-        tmp.push(key);
-        console.log(bagType, 'Found', 'in', key,'which contains', value)
-        }  
+// first we need to parse the data and split into JS object based on 'contains' word:
+tags.replace(/\sbags?\s?/g,'').split(/\.\n/g).forEach( s => {   // ReGex to get rid of repeating word Bag(s) and spaces around it
+  let [a, b] = s.split('contain ');                             // Devide into source and destination bags
+    addNode(a);                                                 // add sources to our adjacencyList
+    b.replace(/\d\s/g,'')                                       // !!!!!!!_____ remove number of bags _____!!!!!!! may come back in part II
+      .split(', ')
+      .forEach( subBag => {
+      if (!subBag.includes('no other')){                        // if the bag can have no other bags => skip
+        addEdge(a,subBag)
+        addParentToChild(a,subBag)
+      }
+    })
+})
+// console.log(adjacencyList)
+
+console.log(compileListOfEventualParents('shiny gold').size)
+
+function compileListOfEventualParents(child){
+  let result = new Set();  // use Set not to revisit same nodes
+  const queue = [child];
+  while (queue.length > 0){
+    console.log(queue)
+    const node = queue.shift();
+    const parentBags = parentalList.get(node);
+    for (let i = 0; parentBags && i < parentBags.length; i++){
+      let parentBag = parentBags[i];
+      if(!result.has(parentBag)){
+        console.log(parentBag)
+        result.add(parentBag);
+        queue.push(parentBag);
+      }
     }
   }
-  return tmp
+
+  return result
 }
